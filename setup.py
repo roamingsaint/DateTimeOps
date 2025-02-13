c487d5417b17bfc75701a71f628857534e6ed416
Kanad Rishiraj <kanad.rishiraj@gmail.com>
2025-02-13 04:10:00 +0000

    setup.py + pyproject.toml (minimal) + MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="datetimeops",
+    version="0.1.0",
+    description="A lightweight Python library for date and time operations.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Kanad Rishiraj (@RoamingSaint)",
+    author_email="roamingsaint27@gmail.com",
+    url="https://github.com/roamingsaint/datetimeops",  # Update with your actual repo
+    packages=find_packages(),
+    install_requires=[
+        "pytz",
+        "python-dateutil",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
